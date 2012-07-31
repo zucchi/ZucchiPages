c997faebb7db7b15db20b2c8998ae813efefa345
Matt Cockayne <matt@zucchi.co.uk>
2012-07-31 12:13:48 +0100

    renamed package

--- Module.php
+++ Module.php
@@ -7,7 +7,7 @@
  * @license   http://framework.zend.com/license/new-bsd New BSD License
  */
 
-namespace ZucchiCms;
+namespace ZucchiPages;
 
 use Zend\ModuleManager\Feature\AutoloaderProviderInterface;
 use Zend\Mvc\ModuleRouteListener;
@@ -31,5 +31,14 @@ class Module implements AutoloaderProviderInterface
     public function getConfig()
     {
         return include __DIR__ . '/config/module.config.php';
+    }
+    
+    public function onBootstrap($e)
+    {
+        // You may not need to do this if you're doing it elsewhere in your
+        // application
+        $eventManager        = $e->getApplication()->getEventManager();
+        $moduleRouteListener = new ModuleRouteListener();
+        $moduleRouteListener->attach($eventManager);
     }
 }
--- config/module.config.php
+++ config/module.config.php
@@ -2,12 +2,50 @@
 return array(
     'controllers' => array(
         'invokables' => array(
-            'skeleton' => 'ZucchiCms\Controller\FrontendController',
+            'zms-frontend' => 'ZucchiPages\Controller\FrontendController',
+        ),
+    ),
+    // default route 
+    'router' => array(
+        'routes' => array(
+            'ZucchiPages' => array(
+                'type'    => 'Literal',
+                'options' => array(
+                    // Change this to something specific to your module
+                    'route'    => '/',
+                    'defaults' => array(
+                        // Change this value to reflect the namespace in which
+                        // the controllers for your module are found
+                        '__NAMESPACE__' => 'ZucchiPages\Controller',
+                        'controller'    => 'FrontendController',
+                        'action'        => 'index',
+                    ),
+                ),
+                'may_terminate' => true,
+                'child_routes' => array(
+                    // This route is a sane default when developing a module;
+                    // as you solidify the routes for your module, however,
+                    // you may want to remove it and replace it with more
+                    // specific routes.
+                    'default' => array(
+                        'type'    => 'Regex',
+                        'options' => array(
+                            'regex'    => '/(?<slug>[a-zA-Z0-9\/_-])',
+                            'defaults' => array(
+                                '__NAMESPACE__' => 'ZucchiPages\Controller',
+                                'controller' => 'zms-frontend',
+                                'format' => 'html',
+                            ),
+                            'spec' => '/%slug%',
+                        ),
+                    ),
+                ),
+            ),
         ),
     ),
     'view_manager' => array(
         'template_path_stack' => array(
-            'ZucchiCms' => __DIR__ . '/../view',
+            'ZucchiPages' => __DIR__ . '/../view',
         ),
     ),
 );
