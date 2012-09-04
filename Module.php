d345b7bd56488da58aa5655e21ef619b34eae20c
Matt Cockayne <matt@zucchi.co.uk>
2012-09-04 18:49:37 +0100

    numerous updates

--- Module.php
+++ Module.php
@@ -4,7 +4,7 @@
  *
  * @link      http://github.com/zendframework/ZendSkeletonModule for the canonical source repository
  * @copyright Copyright (c) 2005-2012 Zucchi Limited (http://zucchi.co.uk)
- * @license   http://framework.zend.com/license/new-bsd New BSD License
+ * @license   http://zucchi.co.uk/legals/bsd-license New BSD License
  */
 
 namespace ZucchiPages;
